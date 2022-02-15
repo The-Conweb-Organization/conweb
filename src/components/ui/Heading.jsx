07d3e0deb912c6156ce007813a979af1fb166000
Stefan Ihle <stefanihle@gmx.net>
2022-02-15 19:18:24 +0100

    solve heading issue

--- src/components/ui/Heading.jsx
+++ src/components/ui/Heading.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 
 const Heading = ({ headingType, isBlogPage, children }) => {
 	return headingType === 'h1' ? (
-		<h1 className='flex justify-between items-center text-center text-conOrange-200 font-bold text-conH2'>
+		<h1 className='flex flex-col md:flex-row md:justify-between items-center text-center text-conOrange-200 font-bold text-conH2'>
 			{children}
 		</h1>
 	) : (
 		<h2
-			className={`flex ${
-				isBlogPage ? 'justify-between' : 'justify-center'
+			className={`flex flex-col md:flex-row ${
+				isBlogPage ? 'md:justify-between' : 'md:justify-center items-center'
 			} items-center text-center text-conOrange-200 font-bold text-conH2`}
 		>
 			{children}
